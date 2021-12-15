5c867dfd3daf454da0d55548cbaf5dac4a8be68e
abdisoftware <abdisofware@gmail.com>
2021-12-15 08:57:15 -0300

    inicio solucion en blanco

---
+++ mitien/mitien.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace mitien.UIClassic
+{
+    public class Class1
+    {
+    }
+}
---
+++ mitien/mitien.common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace mitien.common
+{
+    public class Class1
+    {
+    }
+}
